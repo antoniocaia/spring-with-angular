4b9ac67d14c647e8a220639e9b8234a99ffbedd2
antoniocaia <antonio.caia96@gmail.com>
2021-03-09 10:40:24 +0100

    Changed sed commans and pom1 and pom2
    Fixed a problem when plugins where already defined in the pom

--- spring_angular.sh
+++ spring_angular.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./mvnw install
 cp pom.xml spanc/old_pom.xml
-sed -i '\+<plugins>+r spanc/pom1.txt' pom.xml
+sed '\+<plugins>+r spanc/pom1.txt' spanc/old_pom.xml > pom.xml
 ./mvnw generate-resources
 cp spanc/npm npm
 ./npm install @angular/cli
@@ -10,5 +10,5 @@ cp spanc/setup_angular.sh sa.sh
 ./ng new client
 ./sa.sh
 rm sa.sh
-sed -i '\+</execution>+r spanc/pom2.txt' pom.xml
+sed '\+<plugins>+r spanc/pom2.txt' spanc/old_pom.xml > pom.xml
 ./mvnw generate-resources
